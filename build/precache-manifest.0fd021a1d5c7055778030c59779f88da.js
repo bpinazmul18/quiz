199f87dbe7d39b3de5dc323601976770c4823fd6
Md Nazmul Haque <bpinazmul18@gmail.com>
2021-05-26 10:39:47 +0600

    run production mode

---
+++ build/precache-manifest.0fd021a1d5c7055778030c59779f88da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0698c635773773151ae71a9bac86ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "491edbc4527229b0a974",
+    "url": "/static/css/main.23c27806.chunk.css"
+  },
+  {
+    "revision": "f019e6268483c4df1492",
+    "url": "/static/js/2.35a80486.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.35a80486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "491edbc4527229b0a974",
+    "url": "/static/js/main.ac0d8e10.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
